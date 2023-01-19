bbe4110c1d3f496ca36f5245a4c5954bd5138715
“KarolisBar94“ <karolis.baronas@gmail.com>
2023-01-19 14:22:27 +0200

    ad full project

---
+++ src/data/brands.ts
@@ -0,0 +1,14 @@
+import Brand from '../types/brand';
+
+const brands: Brand[] = [{
+  id: '1',
+  title: 'Ford',
+}, {
+  id: '2',
+  title: 'KIA',
+}, {
+  id: '3',
+  title: 'Audi',
+}];
+
+export default brands;
---
+++ src/data/models.ts
@@ -0,0 +1,41 @@
+import Model from '../types/model';
+
+const models: Model[] = [{
+  id: '1',
+  title: 'Kuga',
+  brandId: '1',
+}, {
+  id: '2',
+  title: 'Mondeo',
+  brandId: '1',
+}, {
+  id: '3',
+  title: 'Galaxy',
+  brandId: '1',
+}, {
+  id: '4',
+  title: 'Sportage',
+  brandId: '2',
+}, {
+  id: '5',
+  title: 'Creed',
+  brandId: '2',
+}, {
+  id: '6',
+  title: 'EV6',
+  brandId: '2',
+}, {
+  id: '7',
+  title: 'S8',
+  brandId: '3',
+}, {
+  id: '8',
+  title: 'ETron',
+  brandId: '3',
+}, {
+  id: '9',
+  title: 'RS6',
+  brandId: '3',
+}];
+
+export default models;
